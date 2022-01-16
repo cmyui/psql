710450b98da7501ee71b728c03deb78c84d1a293
cmyui <cmyuiosu@gmail.com>
2022-01-16 05:45:40 -0500

    add argument parsing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3.9
-"""A program for playing with the postgres 3.0 protocol - for learning purposes.
+"""A command-line interface (currently) for postgresql - for learning learning purposes
 
 I've been wanting to make a database client for a while now, this is pretty fun!
+
+I've left the design open to support other sql dialects (e.g. mysql) in the future,
+and perhaps I'll add support into the codebase in as i go. i want to learn it all!
 
 References
 1. PG Protocol Flow https://www.postgresql.org/docs/14/protocol-flow.html
@@ -11,7 +14,10 @@ __author__ = "Joshua Smith (cmyui)"
 __email__ = "cmyuiosu@gmail.com"
 
 
+import argparse
 import socket
+import sys
+from typing import Sequence
 
 import config
 import handlers
@@ -20,8 +26,11 @@ import log
 import objects
 import packets
 
+VERSION = "0.0.5"
+SUPPORTED_BACKNEDS = ["postgresql"]  # TODO: more!!??
 
-def run_client(server_sock: socket.socket) -> int:
+
+def run_command_line_interface(server_sock: socket.socket) -> int:
     """Run the client until shut down programmatically."""
     log.status(
         f"initiating postgres protocol startup "
@@ -116,21 +125,68 @@ def run_client(server_sock: socket.socket) -> int:
     return 0
 
 
-def main() -> int:
-    # use GNU readline interface
-    import readline  # type: ignore
+def main(argv: Sequence[str]) -> int:
+    parser = argparse.ArgumentParser(prog="pysql")
 
-    # ensure the server is notified
-    # of any client disconnections
-    helpers.setup_shutdown_signal_handlers()
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=f"%(prog)s v{VERSION}",
+    )
 
-    # connect to the postgres server
-    # and run our client until stopped
-    with socket.create_connection(
-        (config.DB_HOST, config.DB_PORT),
-    ) as sock:
-        return run_client(sock)
+    parser.add_argument(
+        "-D",
+        "--debug",
+        action="store_true",
+        help="Enable logging of additional information for debugging purposes",
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    # <> represent required args, () for optional
+
+    # $ pysql cli <backend> (-U/--user) (-H/--host) (-P/--port)
+    cli_parser = subparsers.add_parser("cli", help="Auth into backend and open a cli.")
+    cli_parser.add_argument(
+        "backend",
+        help="The database backend you'd like to connect to (only postgresql)",
+        choices=SUPPORTED_BACKNEDS,
+    )
+    cli_parser.add_argument("-U", "--user", default=None)
+    cli_parser.add_argument("-H", "--host", default=None)
+    cli_parser.add_argument("-P", "--port", default=None)
+
+    if len(argv) == 0:
+        argv = ["--help"]
+
+    args = parser.parse_args(argv)
+
+    if args.debug:
+        config.DEBUG_MODE = True
+
+    if args.command == "cli":
+        """Auth into backend & open a command line interface."""
+        # authenticate & open a command line interface
+        # use GNU readline interface
+        import readline  # type: ignore
+
+        # ensure the server is notified
+        # of any client disconnections
+        helpers.setup_shutdown_signal_handlers()
+
+        # connect to the postgres server
+        # and run our client until stopped
+        with socket.create_connection(
+            (config.DB_HOST, config.DB_PORT),
+        ) as sock:
+            exit_code = run_command_line_interface(sock)
+    else:
+        # XXX: this is impossible to reach
+        exit_code = 1
+
+    return exit_code
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    raise SystemExit(main(sys.argv[1:]))  # [1:] to remove executable
